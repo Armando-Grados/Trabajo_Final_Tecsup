b1f988540b5bea8b8b8008c5cbe5affb9498ffc3
ParvezMia <parvezmia999@hotmail.com>
2022-10-01 05:50:06 +0600

    Fullscreen page loading added

--- src/pages/WelcomePge.jsx
+++ src/pages/WelcomePge.jsx
@@ -2,13 +2,30 @@ import React from "react";
 import Hero from "../components/welcome/hero/Hero";
 import Service from "../components/welcome/service/Service";
 import Testomonial from "../components/welcome/testimonial/Testimonial";
+import ReactDOM from "react-dom";
+import ReactFullpage from "@fullpage/react-fullpage";
 
 const WelcomePge = () => {
   return (
     <>
-      <Hero />
-      <Testomonial />
-      <Service />
+      <ReactFullpage
+        scrollingSpeed={1000} /* Options here */
+        render={(comp) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <div className="section">
+                <Hero />
+              </div>
+              <div className="section">
+                <Testomonial />
+              </div>
+              <div className="section">
+                <Service />
+              </div>
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
     </>
   );
 };
