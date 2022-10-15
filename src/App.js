e3cd29b29de027c65e120357fd2be79d9d6092da
RudyMendoza <pmoises052397@gmail.com>
2022-10-15 08:33:56 -0500

    Update 15/10/22 ProductPage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import PrimaryLayout from './layouts/PrimaryLayout';
 import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 // import { useState } from 'react';
 // import PreLoader from './components/section/PreLoader';
 
@@ -12,7 +13,7 @@ function App() {
     <Routes>
       <Route path='/' element={<PrimaryLayout />}>
         <Route index element={<HomePage />} />
-        <Route></Route>
+        <Route path='/product' element={<ProductPage />}></Route>
         <Route></Route>
       </Route>
     </Routes>
