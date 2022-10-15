9916941e4e9a0bddc10a4561554496e7666acf6c
richard-osorio-rios <richard.osorio.rios@gmail.com>
2022-10-15 03:01:46 -0500

    se agregaron nuevas zapatillas mcon conexion al servicio

--- src/components/home/HomeCamisetas.jsx
+++ src/components/home/HomeCamisetas.jsx
@@ -22,21 +22,11 @@ const HomeCamisetas = () => {
     obtenerListaProd1()
   },[]);
 
-  console.log("aqui");
-  console.log(mini_prod1);
-
     return(
         <section className="section-body">
             <div className="container-galeria container-galeria--elements">
                 <div className="container-galeria elements elements--cards">
-                    <div className="cards">
-                        {/* <img src={require("../../assets/camiseta/adidas-mens-spain-2022-23-authentic-home-jersey-power-red-navy-front_300x.avif")} alt="" id=""/>
-                        <a href="/">Camiseta adidas España 2022/23<br/>Authentic Primera Equipación Hombre<br/>Rojo/Azul Marino<br/> USD 150</a>
-                        <button className="button button--secondary button--card">Agregar al Carrito</button>
-                        <button className="button button--primary button--card">Vista</button> */}
-                       
-                    </div>
-                    {
+                {
                     mini_prod1.map(prod=>{
                     return(
                         <>
@@ -51,64 +41,7 @@ const HomeCamisetas = () => {
                         </>
                         )
                         })
-                    }
-                        
-
-
-                    {/* <div className="cards">
-                        <img src={require("../../assets/camiseta/puma-mens-borussia-dortmund-2022-23-away-jersey-cyber-black-asphalt-front_300x.avif")} alt="" id=""/>
-                        <a href="/">Camiseta adidas España 2022/23<br/>Authentic Primera Equipación Hombre<br/>Rojo/Azul Marino<br/> USD 150</a>
-                        <button className="button button--secondary button--card">Agregar al Carrito</button>
-                        <button className="button button--primary button--card">Vista</button>
-                    </div>
-                    <div className="cards">
-                        <img src={require("../../assets/camiseta/joma-mens-cruz-azul-2022-23-away-jersey-royal-blue-white-front_300x.avif")} alt="" id=""/>
-                        <a href="/">Camiseta adidas España 2022/23<br/>Authentic Primera Equipación Hombre<br/>Rojo/Azul Marino<br/> USD 150</a>
-                        <button className="button button--secondary button--card">Agregar al Carrito</button>
-                        <button className="button button--primary button--card">Vista</button>
-                    </div>
-                    <div className="cards">
-                        <img src={require("../../assets/camiseta/adidas-kids-germany-2022-23-home-jersey-white-black-front_300x.avif")} alt="" id=""/>
-                        <a href="/">Camiseta adidas España 2022/23<br/>Authentic Primera Equipación Hombre<br/>Rojo/Azul Marino<br/> USD 150</a>
-                        <button className="button button--secondary button--card">Agregar al Carrito</button>
-                        <button className="button button--primary button--card">Vista</button>
-                    </div>
-                    <div className="cards">
-                        <img src={require("../../assets/camiseta/adidas-mens-germany-2022-23-away-jersey-black-front_300x.avif")} alt="" id=""/>
-                        <a href="/">Camiseta adidas España 2022/23<br/>Authentic Primera Equipación Hombre<br/>Rojo/Azul Marino<br/> USD 150</a>
-                        <button className="button button--secondary button--card">Agregar al Carrito</button>
-                        <button className="button button--primary button--card">Vista</button>
-                    </div>
-                    <div className="cards">
-                        <img src={require("../../assets/camiseta/adidas-kids-germany-2022-23-away-jersey-black-bronze-front_300x.avif")} alt="" id=""/>
-                        <a href="/">Camiseta adidas España 2022/23<br/>Authentic Primera Equipación Hombre<br/>Rojo/Azul Marino<br/> USD 150</a>
-                        <button className="button button--secondary button--card">Agregar al Carrito</button>
-                        <button className="button button--primary button--card">Vista</button>
-                    </div>
-                    <div className="cards">
-                        <img src={require("../../assets/camiseta/adidas-mens-spain-2022-23-authentic-home-jersey-power-red-navy-front_300x.avif")} alt="" id=""/>
-                        <a href="/">Camiseta adidas España 2022/23<br/>Authentic Primera Equipación Hombre<br/>Rojo/Azul Marino<br/> USD 150</a>
-                        <button className="button button--secondary button--card">Agregar al Carrito</button>
-                        <button className="button button--primary button--card">Vista</button>
-                    </div>
-                    <div className="cards">
-                        <img src={require("../../assets/camiseta/charly-mens-atlas-2022-23-long-sleeve-bi-campeon-jersey-black-red-front_300x.avif")} alt="" id=""/>
-                        <a href="/">Camiseta adidas España 2022/23<br/>Authentic Primera Equipación Hombre<br/>Rojo/Azul Marino<br/> USD 150</a>
-                        <button className="button button--secondary button--card">Agregar al Carrito</button>
-                        <button className="button button--primary button--card">Vista</button>
-                    </div>
-                    <div className="cards">
-                        <img src={require("../../assets/camiseta/puma-mens-senegal-2022-23-home-jersey-white-red-front_300x.avif")} alt="" id=""/>
-                        <a href="/">Camiseta adidas España 2022/23<br/>Authentic Primera Equipación Hombre<br/>Rojo/Azul Marino<br/> USD 150</a>
-                        <button className="button button--secondary button--card">Agregar al Carrito</button>
-                        <button className="button button--primary button--card">Vista</button>
-                    </div>
-                    <div className="cards">
-                        <img src={require("../../assets/camiseta/nike-mens-saudi-arabia-2023-23-away-jersey-malachite-white-front_300x.avif")} alt="" id=""/>
-                        <a href="/">Camiseta adidas España 2022/23<br/>Authentic Primera Equipación Hombre<br/>Rojo/Azul Marino<br/> USD 150</a>
-                        <button className="button button--secondary button--card">Agregar al Carrito</button>
-                        <button className="button button--primary button--card">Vista</button>
-                    </div> */}
+                }        
                 </div>
             </div>
         </section>
--- src/components/home/HomeZapatillas.jsx
+++ src/components/home/HomeZapatillas.jsx
@@ -16,6 +16,10 @@ const HomeZapatillas = () => {
   const CONTROLLER_PROD4 = 'producto/11';
   const CONTROLLER_PROD5 = 'producto/14';
   const CONTROLLER_PROD6 = 'producto/17';
+  const CONTROLLER_PROD7 = 'producto/29';
+  const CONTROLLER_PROD8 = 'producto/31';
+  const CONTROLLER_PROD9 = 'producto/33';
+  const CONTROLLER_PROD10 = 'producto/35';
 
   // URL LISTADO MODELOS POR PRODUCTO 
 
@@ -25,6 +29,10 @@ const HomeZapatillas = () => {
   const CONTROLLER_LIST_PROD4 = 'producto/getbyall/1/4';
   const CONTROLLER_LIST_PROD5 = 'producto/getbyall/1/5';
   const CONTROLLER_LIST_PROD6 = 'producto/getbyall/1/6';
+  const CONTROLLER_LIST_PROD7 = 'producto/getbyall/1/7';
+  const CONTROLLER_LIST_PROD8 = 'producto/getbyall/1/8';
+  const CONTROLLER_LIST_PROD9 = 'producto/getbyall/1/9';
+  const CONTROLLER_LIST_PROD10 = 'producto/getbyall/1/10';
 
   //useEffect
   const [ prod1, setProd1 ] = useState([]);
@@ -33,6 +41,10 @@ const HomeZapatillas = () => {
   const [ prod4, setProd4 ] = useState([]);
   const [ prod5, setProd5 ] = useState([]);
   const [ prod6, setProd6 ] = useState([]);
+  const [ prod7, setProd7 ] = useState([]);
+  const [ prod8, setProd8 ] = useState([]);
+  const [ prod9, setProd9 ] = useState([]);
+  const [ prod10, setProd10 ] = useState([]);
 
   useEffect(()=>{
     const obtenerProd1 = async ()=>{
@@ -94,12 +106,56 @@ const HomeZapatillas = () => {
     obtenerProd6()
   },[]);
 
+  useEffect(()=>{
+    const obtenerProd7 = async ()=>{
+      const url = URL_BASE + CONTROLLER_PROD7;
+      const result = await axios.get(url);
+      setProd7(result.data)
+    }
+
+    obtenerProd7()
+  },[]);
+
+  useEffect(()=>{
+    const obtenerProd8 = async ()=>{
+      const url = URL_BASE + CONTROLLER_PROD8;
+      const result = await axios.get(url);
+      setProd8(result.data)
+    }
+
+    obtenerProd8()
+  },[]);
+
+  useEffect(()=>{
+    const obtenerProd9 = async ()=>{
+      const url = URL_BASE + CONTROLLER_PROD9;
+      const result = await axios.get(url);
+      setProd9(result.data)
+    }
+
+    obtenerProd9()
+  },[]);
+
+  useEffect(()=>{
+    const obtenerProd10 = async ()=>{
+      const url = URL_BASE + CONTROLLER_PROD10;
+      const result = await axios.get(url);
+      setProd10(result.data)
+    }
+
+    obtenerProd10()
+  },[]);
+
   const [ mini_prod1, setMiniProd1 ] = useState([]);
   const [ mini_prod2, setMiniProd2 ] = useState([]);
   const [ mini_prod3, setMiniProd3 ] = useState([]);
   const [ mini_prod4, setMiniProd4 ] = useState([]);
   const [ mini_prod5, setMiniProd5 ] = useState([]);
   const [ mini_prod6, setMiniProd6 ] = useState([]);
+  const [ mini_prod7, setMiniProd7 ] = useState([]);
+  const [ mini_prod8, setMiniProd8 ] = useState([]);
+  const [ mini_prod9, setMiniProd9 ] = useState([]);
+  const [ mini_prod10, setMiniProd10 ] = useState([]);
 
   useEffect(()=>{
     const obtenerListaProd1 = async ()=>{
@@ -153,6 +209,42 @@ const HomeZapatillas = () => {
       setMiniProd6(result.data)
     }
     obtenerListaProd6()
+  },[]);
+
+  useEffect(()=>{
+    const obtenerListaProd7 = async ()=>{
+      const url = URL_BASE + CONTROLLER_LIST_PROD7;
+      const result = await axios.get(url);
+      setMiniProd7(result.data)
+    }
+    obtenerListaProd7()
+  },[]);
+
+  useEffect(()=>{
+    const obtenerListaProd8 = async ()=>{
+      const url = URL_BASE + CONTROLLER_LIST_PROD8;
+      const result = await axios.get(url);
+      setMiniProd8(result.data)
+    }
+    obtenerListaProd8()
+  },[]);
+
+  useEffect(()=>{
+    const obtenerListaProd9 = async ()=>{
+      const url = URL_BASE + CONTROLLER_LIST_PROD9;
+      const result = await axios.get(url);
+      setMiniProd9(result.data)
+    }
+    obtenerListaProd9()
+  },[]);
+
+  useEffect(()=>{
+    const obtenerListaProd10 = async ()=>{
+      const url = URL_BASE + CONTROLLER_LIST_PROD10;
+      const result = await axios.get(url);
+      setMiniProd10(result.data)
+    }
+    obtenerListaProd10()
   },[]);
 
 
@@ -249,12 +341,79 @@ const HomeZapatillas = () => {
             <button className="button button--primary button--card">Vista</button>
           </div>
           <div className="cards">
-            {/* <img src={require("../../assets/catalogo/Nike_Tiempo_ Legend_9_Academy_TF/nike-mens-tiempo-legend-9-academy-tf-sail-light-brown.webp")} alt="" id="card_principal-6"/> */}
             <img src={prod6.Ruta_Imagen_Grande} alt="" id="card_principal-6"/>
             <a href="/">Nike Phantom GT2 Elite FG Hombre<br/>Gris/Azul Ennegrecido<br/> USD 250</a>
             <div className="cards card-view-item6">
             {
                 mini_prod6.map(prod=>{
+                  return(
+                    <div key={prod.idproducto}>
+                      <img src={prod.Ruta_Imagen_Pequeno} className={prod.Clase}/>
+                    </div>
+                    )
+                })
+            }
+            </div>
+            <button className="button button--secondary button--card">Agregar al Carrito</button>
+            <button className="button button--primary button--card">Vista</button>
+          </div>
+          <div className="cards">
+            <img src={prod7.Ruta_Imagen_Grande} alt="" id="card_principal-7"/>
+            <a href="/">Nike Phantom GT2 Elite FG Hombre<br/>Gris/Azul Ennegrecido<br/> USD 250</a>
+            <div className="cards card-view-item7">
+            {
+                mini_prod7.map(prod=>{
+                  return(
+                    <div key={prod.idproducto}>
+                      <img src={prod.Ruta_Imagen_Pequeno} className={prod.Clase}/>
+                    </div>
+                    )
+                })
+            }
+            </div>
+            <button className="button button--secondary button--card">Agregar al Carrito</button>
+            <button className="button button--primary button--card">Vista</button>
+          </div>
+          <div className="cards">
+            <img src={prod8.Ruta_Imagen_Grande} alt="" id="card_principal-8"/>
+            <a href="/">Nike Phantom GT2 Elite FG Hombre<br/>Gris/Azul Ennegrecido<br/> USD 250</a>
+            <div className="cards card-view-item8">
+            {
+                mini_prod8.map(prod=>{
+                  return(
+                    <div key={prod.idproducto}>
+                      <img src={prod.Ruta_Imagen_Pequeno} className={prod.Clase}/>
+                    </div>
+                    )
+                })
+            }
+            </div>
+            <button className="button button--secondary button--card">Agregar al Carrito</button>
+            <button className="button button--primary button--card">Vista</button>
+          </div>
+          <div className="cards">
+            <img src={prod9.Ruta_Imagen_Grande} alt="" id="card_principal-9"/>
+            <a href="/">Nike Phantom GT2 Elite FG Hombre<br/>Gris/Azul Ennegrecido<br/> USD 250</a>
+            <div className="cards card-view-item9">
+            {
+                mini_prod9.map(prod=>{
+                  return(
+                    <div key={prod.idproducto}>
+                      <img src={prod.Ruta_Imagen_Pequeno} className={prod.Clase}/>
+                    </div>
+                    )
+                })
+            }
+            </div>
+            <button className="button button--secondary button--card">Agregar al Carrito</button>
+            <button className="button button--primary button--card">Vista</button>
+          </div>
+          <div className="cards">
+            <img src={prod10.Ruta_Imagen_Grande} alt="" id="card_principal-10"/>
+            <a href="/">Nike Phantom GT2 Elite FG Hombre<br/>Gris/Azul Ennegrecido<br/> USD 250</a>
+            <div className="cards card-view-item10">
+            {
+                mini_prod10.map(prod=>{
                   return(
                     <div key={prod.idproducto}>
                       <img src={prod.Ruta_Imagen_Pequeno} className={prod.Clase}/>
